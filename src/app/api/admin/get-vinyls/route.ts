1d549896305feec85dabaa6306b441a4f31946b6
Adrian Dever <ad.dever@yahoo.com>
2025-04-11 11:16:54 +0200

    fix

--- src/app/api/admin/get-vinyls/route.ts
+++ src/app/api/admin/get-vinyls/route.ts
@@ -1,13 +1,8 @@
 // src/app/api/admin/get-vinyls/route.ts
-import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
+import { supabase } from '@/lib/supabase/client';
 
 export async function GET() {
-    const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
-
     const { data: vinyls, error } = await supabase
         .from("Vinyl")
         .select("id, title, artist, genre, price, image");
